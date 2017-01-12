51a972419900b85053e68b74951739c6224da52e
JaysQubeXon <noam.jacobson.business@gmail.com>
2017-01-12 17:17:00 +0200

    using express and express-graphql for graphiql interface and server

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'user strict';
 
+const express = require('./graphql-server/node_modules/express');//: web framework to serve up the graphql server
+const graphqlHTTP = require('./graphql-server/node_modules/express-graphql');//: allowing us to serve up the graphql schema as a middleware in express
 const { graphql, buildSchema } = require('./graphql-server/node_modules/graphql');
-// describe the capabilities of the graphql server with schema
+
+const PORT = process.env.PORT || 3000;
+const server = express();
+
 const schema = buildSchema(`
   type Video {
     id: ID,
@@ -11,7 +16,7 @@ const schema = buildSchema(`
   }
 
   type Query {
-    video: Video,
+    video: Video
     videos: [Video]
   }
 
@@ -34,42 +39,41 @@ const videoB = {
   watched: false,
 };
 
-const videos = [videoA, videoB];//collection of videos
+const videos = [videoA, videoB];
 
 //create resolver:
-const resolvers = {//resolvers for the schema
+const resolvers = {
   video: () => ({
     id: () => '1',
     title: () => 'Foo',
     duration: () => 180,
     watched: () => true,
-  }),// need to update resolvers. now we have a videos field  need to tell
-       // our GraphQL Schema how to resolve the videos field exactly:
+  }),
+
     videos: () => videos,
 };
-// need to update video to videos by adding one letter:
-const query = `
-query myFirstQuery {
-  video {
-    id,
-    title,
-    duration,
-    watched
-  }
-  videos {
-    id,
-    title,
-    duration,
-    watched
-  }
-}
-`;
-// run our query agenst the schema we had defined and including our resolvers:
-graphql(schema, query, resolvers)
-  .then((result) => console.log(result))// promise
-  .catch((error) => console.log(error));
+
+
+//can get rid off the graphql utility function and write instead:
+server.use('/graphql', graphqlHTTP({//: mount it on the graphql endpoint and pass in the graphqlHTTP middleware function
+  schema,// pass in configuration objects. schema corresponds to the schema that we are constructing above
+  graphiql: true,//: the second option: for using a tool called graphiql = a virual editor for graphql schemas
+  rootValue: resolvers, //: similar to how we used the graphql utility function
+}));                    //and passed resolvers that is defined up above, that defines
+                        // how to fetch some of these fields.
+
+
+server.listen(PORT, () => {
+  console.log(`Listeing on http://localhost:${PORT}`);
+});
 
 /*write in git bash terminal: node <file-name.js>
-$ node gql_lesson4.js
-{ data: { video: { id: '1', title: 'Foo', duration: 180, watched: true },
-{ data: { videos: [ [Object], [Object] ] } }*/
+$ node gql_lesson5.js =>> will bootstrap the server
+response:
+Listeing on http://localhost:3000
+
+if you go into the browser and look up:
+http://localhost:3000/graphql ==> where the middleware is hosted on
+and get a graphical view of the tool called GraphiQL that will allow querying
+the Schema
+*/
